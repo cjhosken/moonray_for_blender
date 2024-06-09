10385e99db4848e2c288f38614ad081a642bc2eb
cjhosken <hoskenchristopher@gmail.com>
2024-06-09 01:15:26 +0100

    issues with half  root

---
+++ install_moonray_rhel9.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+
+OS_DIST="RHEL9"
+
+# Function to update package manager and install required dependencies
+update_and_install() {
+  local package_manager=$1
+
+  case $package_manager in
+    apt-get)
+      $package_manager update
+      $package_manager install -y git cmake build-essential libtbb-dev
+      ;;
+    yum)
+      $package_manager update -y
+      $package_manager install -y git cmake gcc-c++ make tbb-devel
+      ;;
+    *)
+      echo "Unsupported package manager: $package_manager"
+      exit 1
+      ;;
+  esac
+}
+
+# Check which package manager is available
+if command_exists apt-get; then
+  echo "Detected Ubuntu/Debian-based system"
+  update_and_install "apt-get"
+elif command_exists yum; then
+  echo "Detected Red Hat/CentOS-based system"
+  update_and_install "yum"
+else
+  echo "Unable to determine the package manager. Please install the required dependencies manually."
+  exit 1
+fi
+
+# Get the non-sudo user's home directory
+non_sudo_home_dir=$(eval echo "~$SUDO_USER")
+script_dir=/home/cjhosken/Documents/programming/moonray_for_blender
+
+# Define the installation directory
+INSTALL_DIR=$non_sudo_home_dir/dreamworks
+SOURCE_DIR=$INSTALL_DIR/source
+MOONRAY_DIR=$INSTALL_DIR/moonray
+
+BUILD_DIR=$INSTALL_DIR/build
+
+
+if [ ! -d "$SOURCE_DIR" ]; then
+  mkdir -p "$INSTALL_DIR"
+  chown -R "$SUDO_USER:$SUDO_USER" "$INSTALL_DIR"
+  chmod -R 755 "$INSTALL_DIR"
+fi
+
+if [ -d "$SOURCE_DIR" ]; then
+    read -p "Source directory already exists. Do you want to override it? (y/n): " answer
+    case ${answer,,} in
+        y)
+            rm -rf "$SOURCE_DIR"
+            git clone --recurse-submodules https://github.com/dreamworksanimation/openmoonray.git "$SOURCE_DIR"
+            ;;
+        n)
+            echo "Continuing with current source..."
+          
+            ;;
+        *)
+            echo "Invalid input. Exiting..."
+            exit 1
+            ;;
+    esac
+else
+git clone --recurse-submodules https://github.com/dreamworksanimation/openmoonray.git "$SOURCE_DIR"
+fi
+
+# Change ownership and permissions
+chown -R "$SUDO_USER:$SUDO_USER" "$SOURCE_DIR"
+chmod -R 755 "$SOURCE_DIR"
+
+if [ ! -d "$BUILD_DIR" ]; then
+mkdir -p "$BUILD_DIR"
+fi
+chown -R "$SUDO_USER:$SUDO_USER" "$BUILD_DIR"
+chmod -R 755 "$BUILD_DIR"
+
+
+cd "$BUILD_DIR"
+
+if [ -d "$INSTALL_DIR/source/building/$OS_DIST" ]; then
+rm -rf "$INSTALL_DIR/source/building/$OS_DIST"
+fi
+
+cp -r "$script_dir/moonray/$OS_DIST/" "$INSTALL_DIR/source/building/$OS_DIST/"
+chown -R "$SUDO_USER:$SUDO_USER" "$INSTALL_DIR/source/building/$OS_DIST"
+chmod -R 755 "$INSTALL_DIR/source/building/$OS_DIST"
+
+if [ -d "/installs" ]; then
+rm -rf "/installs"
+fi
+
+mkdir -p /installs
+
+cd "$BUILD_DIR"
+
+source "$SOURCE_DIR/building/$OS_DIST/install_packages.sh"
+
+echo "Using Cmake Version"
+cmake --version
+
+echo "Building Dependencies..."
+
+cmake "$SOURCE_DIR/building/$OS_DIST/"
+cmake --build . -- -j $(nproc)
+
+chown -R "$SUDO_USER:$SUDO_USER" "$BUILD_DIR"
+chmod -R 755 "$BUILD_DIR"
+
+echo "Dependencies Built!"
+
+
+echo "Loading Optix Headers"
+
+if [ ! -f "/usr/local/include/optix.h" ]; then
+cd "/usr/local/"
+bash "$SOURCE_DIR/building/$OS_DIST/install_optix.sh" --skip-license
+fi
+
+cd "$BUILD_DIR"
+
+echo "Optix Headers Loaded!"
+
+echo "Building MoonRay..."
+
+cmake "$SOURCE_DIR" -DPYTHON_EXECUTABLE=python3 -DBOOST_PYTHON_COMPONENT_NAME=python39 -DABI_VERSION=0 -DMOONRAY_USE_CUDA=YES
+cmake --build . -- -j $(nproc)
+
+echo "MoonRay Built!"
+
+echo "Installing Moonray..."
+
+if [ ! -d "$MOONRAY_DIR" ]; then
+mkdir "$MOONRAY_DIR"
+fi
+
+cmake --install . --prefix "$MOONRAY_DIR"
+
+
+# Set ownership and permissions for installed files
+chown -R "$SUDO_USER:$SUDO_USER" "$MOONRAY_DIR"
+chmod -R 755 "$MOONRAY_DIR"
+
+echo "MoonRay installation completed successfully!"
+
+
+echo "Running Test"
+
+source "$MOONRAY_DIR/scripts/setup.sh"
+moonray -in $SOURCE_DIR/testdata/rectangle.rdla -out /tmp/rectangle.exr
---
+++ moonray/RHEL9/install_packages.sh
@@ -0,0 +1,69 @@
+# Copyright 2023 DreamWorks Animation LLC
+# SPDX-License-Identifier: Apache-2.0 
+
+# Install Rocky Linux 9 packages for building MoonRay
+# source this script in bash
+
+install_qt=1
+install_cuda=1
+for i in "$@" 
+do
+case ${i,,} in
+    --noqt|-noqt)
+        install_qt=0
+    ;;
+    --nocuda|-nocuda)
+        install_cuda=0
+    ;;
+    *)
+        echo "Unknown option: $i"
+        return 1
+    ;;
+esac
+done
+
+
+dnf install -y epel-release
+
+# not required if you are not building with GPU support
+if [ $install_cuda -eq 1 ] 
+then
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
+    dnf install -y cuda-runtime-11-8 cuda-toolkit-11-8
+fi
+
+dnf install -y libglvnd-devel
+
+dnf install -y gcc gcc-c++
+
+dnf install -y bison flex wget git python3 python3-devel patch \
+               giflib-devel libmng libtiff-devel libjpeg-devel \
+               libatomic libuuid-devel openssl-devel curl-devel \
+               freetype-devel zlib-devel
+
+dnf install -y lsb_release
+
+mkdir -p /installs/{bin,lib,include}
+cd /installs
+
+wget https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-linux-x86_64.tar.gz
+tar xzf cmake-3.23.1-linux-x86_64.tar.gz
+rm -rf /installs/cmake-3.23.1-linux-x86_64.tar.gz
+
+dnf install -y blosc blosc-devel #1.21.2
+dnf install -y boost boost-chrono boost-date-time boost-filesystem boost-python3 boost-program-options boost-regex boost-thread boost-system boost-devel #1.75.0
+dnf install -y lua lua-libs lua-devel #5.4.4
+dnf install -y openvdb openvdb-libs openvdb-devel #9.1.0
+dnf install -y tbb tbb-devel python3-tbb #2020.3
+dnf install -y log4cplus log4cplus-devel #2.0.5
+dnf install -y cppunit cppunit-devel #1.15.1
+dnf install -y libmicrohttpd libmicrohttpd-devel #0.9.72
+
+# not required if you are not building the GUI apps
+if [ $install_qt -eq 1 ]
+then
+    dnf install -y qt5-qtbase-devel qt5-qtscript-devel
+fi
+
+export PATH=/installs/cmake-3.23.1-linux-x86_64/bin:/usr/local/cuda/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
