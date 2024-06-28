68f8fc1d06c0751bf581e9ab67dbddd40334e724
Christopher Hosken <hoskenchristopher@gmail.com>
2024-06-28 18:05:17 +0100

    update for blender 4.2

--- __init__.py
+++ __init__.py
@@ -1,31 +1,15 @@
-#  MoonRay for Blender is a render engine addon which allows users to utilise Dreamwork's MoonRay Production Renderer inside of Blender.
-#  Copyright 2024 Christopher Hosken
-# 
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import bpy
 
 bl_info = {
     "name" : "MoonRay For Blender",
     "author" : "Christopher Hosken",
     "version" : (1, 5, 0, 0),
-    "blender" : (4, 1, 0),
-    "description" : "Dreamworks' MoonRay Production Renderer integration",
+    "blender" : (4, 2, 0),
+    "description" : "Dreamworks' MoonRay Production Renderer integration into Blender",
     "warning" : "This Addon is currently under development",
     "support": "COMMUNITY",
-    "doc_url": "https:bl//github.com/Christopher-Hosken/moonray_for_blender/wiki",
-    "tracker_url": "https://github.com/Christopher-Hosken/moonray_for_blender/issues",
+    "doc_url": "https:bl//github.com/cjhosken/moonray_for_blender/wiki",
+    "tracker_url": "https://github.com/cjhosken/moonray_for_blender/issues",
     "category" : "Render"
 }
 
---
+++ building/Win11/install_packages.bat
@@ -0,0 +1,79 @@
+# Copyright 2023 DreamWorks Animation LLC
+# SPDX-License-Identifier: Apache-2.0 
+
+# Install Rocky Linux 9 packages for building MoonRay
+# source this script in bash
+
+install_qt=1
+install_cuda=1
+for i in "$@" 
+do
+case ${i,,} in
+    --noqt|-noqt)
+        install_qt=0
+    ;;
+    --nocuda|-nocuda)
+        install_cuda=0
+    ;;
+    *)
+        echo "Unknown option: $i"
+        return 1
+    ;;
+esac
+done
+
+
+dnf install -y epel-release
+dnf config-manager --enable crb
+
+# not required if you are not building with GPU support
+if [ $install_cuda -eq 1 ] 
+then
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
+    dnf install -y cuda-runtime-12-1 cuda-toolkit-12-1
+fi
+
+dnf install -y libglvnd-devel
+
+dnf install -y gcc gcc-c++
+
+dnf install -y bison flex wget git python3 python3-devel patch \
+               giflib-devel libmng libtiff-devel libjpeg-devel \
+               libatomic libuuid-devel openssl-devel curl-devel \
+               freetype-devel zlib-devel
+
+dnf install -y lsb_release
+
+mkdir -p ./installs/{bin,lib,include}
+cd ./installs
+
+CMAKE_VERSION="3.29.4"
+
+wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-linux-x86_64.tar.gz
+tar xzf cmake$CMAKE_VERSION-linux-x86_64.tar.gz
+rm -rf ./installs/cmake-$CMAKE_VERSION-linux-x86_64.tar.gz
+
+# Installing Boost
+wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz
+tar -xzf boost_1_75_0.tar.gz
+cd boost_1_75_0
+./bootstrap.sh --prefix=/usr/local/
+./b2 install
+
+dnf install -y blosc blosc-devel #1.21.2
+dnf install -y boost boost-chrono boost-date-time boost-filesystem boost-python3 boost-program-options boost-regex boost-thread boost-system boost-devel #1.75.0
+dnf install -y lua lua-libs lua-devel #5.4.4
+dnf install -y openvdb openvdb-libs openvdb-devel #9.1.0
+dnf install -y tbb tbb-devel python3-tbb #2020.3
+dnf install -y log4cplus log4cplus-devel #2.0.5
+dnf install -y cppunit cppunit-devel #1.15.1
+dnf install -y libmicrohttpd libmicrohttpd-devel #0.9.72
+
+# not required if you are not building the GUI apps
+if [ $install_qt -eq 1 ]
+then
+    dnf install -y qt5-qtbase-devel qt5-qtscript-devel
+fi
+
+export PATH="./installs/cmake-$CMAKE_VERSION-linux-x86_64/bin:/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
