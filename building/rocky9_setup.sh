979c2f176cfaf42619d2b77982191de0a6329cab
Christopher Hosken <hoskenchristopher@gmail.com>
2024-08-06 17:28:02 +0100

    change to mfb

--- building/rocky9_setup.sh
+++ building/rocky9_setup.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-mkdir "$HOME/.dreamworks"
-cd "$HOME/.dreamworks"
+mkdir "$HOME/.mfb"
+cd "$HOME/.mfb"
 
 git clone --recurse-submodules https://github.com/dreamworksanimation/openmoonray.git ./source
 
@@ -14,7 +14,7 @@ sudo -s source ./source/building/Rocky9/install_packages.sh
 cp -r /installs/* ./dependencies
 sudo rm -rf /installs
 
-export PATH=$HOME/.dreamworks/dependencies/cmake-3.23.1-linux-x86_64/bin:/usr/local/cuda/bin:${PATH}
+export PATH=$HOME/.mfb/dependencies/cmake-3.23.1-linux-x86_64/bin:/usr/local/cuda/bin:${PATH}
 
 cd ./dependencies
 cp $SCRIPT_DIR/optix.sh ./optix.sh
@@ -25,11 +25,11 @@ rm -rf ./build
 mkdir ./build
 cd ./build
 
-cmake ../source/building/Rocky9 -DInstallRoot="$HOME/.dreamworks/dependencies"
+cmake ../source/building/Rocky9 -DInstallRoot="$HOME/.mfb/dependencies"
 cmake --build . -- -j $(nproc)
 
 rm -rf *
-cmake ../source -DPYTHON_EXECUTABLE=python3 -DBOOST_PYTHON_COMPONENT_NAME=python39 -DABI_VERSION=0 -DCMAKE_PREFIX_PATH="$HOME/.dreamworks/dependencies"
+cmake ../source -DPYTHON_EXECUTABLE=python3 -DBOOST_PYTHON_COMPONENT_NAME=python39 -DABI_VERSION=0 -DCMAKE_PREFIX_PATH="$HOME/.mfb/dependencies"
 cmake --build . -j $(nproc)
 
 rm -rf ../installs
